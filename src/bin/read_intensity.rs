79af9f0beef61200e9afa29aaf093841a70f5967
Ben Potter <bp@benpot.com>
2025-07-14 11:21:05 -0400

    support choosing reference frame in src/bin/read_intensity.rs

--- src/bin/read_intensity.rs
+++ src/bin/read_intensity.rs
@@ -1,32 +1,49 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use image::ImageReader;
 use rayon::prelude::*;
 use rumpus::image::{to_rgb, IntensityImage, StokesReferenceFrame};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    path: String,
+    /// Path to the input image.
+    image: PathBuf,
 
-    #[arg(short, long, default_value_t = 0.5)]
+    #[arg(long, default_value_t = 0.5)]
     dop_max: f64,
+
+    #[arg(value_enum)]
+    frame: ReferenceFrame,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum ReferenceFrame {
+    /// Incident light reference frame.
+    Pixel,
+
+    /// Sensor reference frame.
+    Sensor,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let image = ImageReader::open(args.path)
+    let image = ImageReader::open(args.image)
         .unwrap()
         .decode()
         .unwrap()
         .into_luma8();
 
     let (width, height) = image.dimensions();
+    let frame = match args.frame {
+        ReferenceFrame::Pixel => StokesReferenceFrame::Pixel,
+        ReferenceFrame::Sensor => StokesReferenceFrame::Sensor,
+    };
     let stokes_image = IntensityImage::from_bytes(width, height, &image.into_raw())
         .unwrap()
         .into_stokes_image()
-        .par_transform_frame(StokesReferenceFrame::Pixel);
+        .par_transform_frame(frame);
 
     let mms = stokes_image.into_measurements();
 
